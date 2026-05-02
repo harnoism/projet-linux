168a74ea65c45725934bf4a866d8b587cdcf029f
harnoism <harnois.maevaa@gmail.com>
2026-05-02 19:11:36 +0200

    CSV file complete, along with the penalty for length and spaces

---
+++ mark.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+note=20
+DOSSIER=$1
+
+IFS='_' read -r -a tableau <<< "$DOSSIER"
+nom=${tableau[0]}
+prenom=${tableau[1]}
+
+
+verifier_longueur_lignes() {
+    fichier=$1
+    too_long=0
+
+    while IFS= read -r ligne; do
+        length=${#ligne}
+        if [ "$length" -gt 80 ]; then
+            too_long=1
+        fi
+    done < "$fichier"
+
+    if [ "$too_long" -eq 1 ]; then
+        echo "Code de plus de 80 caractères détecté dans $fichier"
+        note=$((note - 2))
+    fi
+}
+
+
+verifier_espaces(){
+    fichier=$1
+    mal_indente=0
+
+    while IFS= read -r ligne; do
+        [ -z "$ligne" ] && continue #ignore le vide
+        [[ "$ligne" == "}"* ]] && continue #ignore les {}
+
+        espaces=$(echo "$ligne" | sed 's/[^ ].*//') #supprime tout sauf les espaces du début
+        nb_espaces=${#espaces}
+        
+        if [ $nb_espaces -gt 0 ] && [ $((nb_espaces %2)) -ne 0 ]; then
+            mal_indente=1
+        fi
+
+    done < "$fichier"
+
+    if [ $mal_indente -eq 1 ]; then
+        echo "Mauvaise indentation dans $fichier"
+        note=$((note - 2))
+    fi           
+}
+
+verifier_longueur_lignes "main.c"
+verifier_longueur_lignes "header.h"
+verifier_espaces "main.c"
+verifier_espaces "header.h"
+
+echo "Note finale de $prenom $nom : $note/20"
+
+if [ ! -f "note.csv" ]; then
+    echo "Nom,Prénom,Note" > "note.csv"
+fi
+echo "'$nom','$prenom',$note" >> "note.csv"
